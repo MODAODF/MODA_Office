LibreOffice/core

tdf#155462: fix the scrollbar tooltip text
Over the time when it was completely unused, it regressed in a couple
of aspects:

1. It got assembled in incorrect order: instead of appending chapter,
   it got prepended in commit 832e5aadbff006ec24959162c29756fe2b1982be
   (Related: fdo#38838 remove UniString::SearchAndReplaceAll, 2013-10-08);
2. It started to show chapters, only when the respective heading are
   at the very top of screen, and show only page elsewhere, likely in
   commit 835cd06a047717dfe5e0f117959f3c042e13b21b (tdf#38093 Writer
   outline folding - outline visibility and on canvas ui, 2020-07-30),
   where a call to SwNode::FindOutlineNodeOfLevel was replaced with
   SwOutlineNodes::Seek_Entry in SwCursorShell::GetContentAtPos.

Change-Id: I3f427f7ecb3b6c58a441220c555b22e765a533c4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154077
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sw/inc/crsrsh.hxx
@@ -88,15 +88,16 @@ enum class IsAttrAtPos
     SmartTag         = 0x0800,
     FormControl      = 0x1000,
     TableRedline     = 0x2000,
-    TableColRedline  = 0x4000
+    TableColRedline  = 0x4000,
 #ifdef DBG_UTIL
-    ,CurrAttrs       = 0x8000        ///< only for debugging
-    ,TableBoxValue   = 0x10000       ///< only for debugging
+    CurrAttrs       = 0x8000,        ///< only for debugging
+    TableBoxValue   = 0x10000,       ///< only for debugging
 #endif
-    , ContentControl = 0x20000
+    ContentControl = 0x20000,
+    AllowContaining = 0x40000, // With Outline, finds an outline node for non-outline position
 };
 namespace o3tl {
-    template<> struct typed_flags<IsAttrAtPos> : is_typed_flags<IsAttrAtPos, 0x3ffff> {};
+    template<> struct typed_flags<IsAttrAtPos> : is_typed_flags<IsAttrAtPos, 0x7ffff> {};
 }
 
 struct SwContentAtPos

MODAODF/MODA_Office

(cherry picked from commit 7e997097eb0e36bbb6f1eb8519acfc4e8eb6337a)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154174
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/154166
Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

sw/inc/crsrsh.hxx
@@ -87,15 +87,16 @@ enum class IsAttrAtPos
     ContentCheck     = 0x0400,
     SmartTag         = 0x0800,
     FormControl      = 0x1000,
-    TableRedline     = 0x2000
+    TableRedline     = 0x2000,
 #ifdef DBG_UTIL
-    ,CurrAttrs       = 0x4000        ///< only for debugging
-    ,TableBoxValue   = 0x8000        ///< only for debugging
+    CurrAttrs       = 0x4000,        ///< only for debugging
+    TableBoxValue   = 0x8000,        ///< only for debugging
 #endif
-    , ContentControl = 0x10000
+    ContentControl = 0x10000,
+    AllowContaining = 0x20000, // With Outline, finds an outline node for non-outline position
 };
 namespace o3tl {
-    template<> struct typed_flags<IsAttrAtPos> : is_typed_flags<IsAttrAtPos, 0x1ffff> {};
+    template<> struct typed_flags<IsAttrAtPos> : is_typed_flags<IsAttrAtPos, 0x3ffff> {};
 }
 
 struct SwContentAtPos