0b661f4c68da3380943ae5436c27173a5481ebe8
Firefly <firefly@ossii.com.tw>
2023-09-26 14:19:26 +0800

    [MODA] 增加 WITH_MODA_EXTRA_EXTENSIONS 環境變數.
    避免 --enable-mpl-subset，造成打包錯誤.
    
    Change-Id: I5d8cd8439438c365615c411f769591ef107c51ff

--- external/Module_external.mk
+++ external/Module_external.mk
@@ -73,6 +73,7 @@ $(eval $(call gb_Module_add_moduledirs,external,\
 	$(call gb_Helper_optional,MDDS,mdds) \
 	$(call gb_Helper_optional,MDNSRESPONDER,mdnsresponder) \
 	$(if $(WITH_EXTRA_EXTENSIONS),misc_extensions) \
+	$(if $(WITH_MODA_EXTRA_EXTENSIONS),misc_extensions) \
 	$(call gb_Helper_optional,MORE_FONTS,more_fonts) \
 	$(call gb_Helper_optional,MSPUB,libmspub) \
 	$(call gb_Helper_optional,MWAW,libmwaw) \
