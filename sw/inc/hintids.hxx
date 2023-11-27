LibreOffice/core

sw floattable: add per-frame wrap-on-all-pages mode
Currently the wrap-on-all pages mode is off by default and a
document-level setting can enable it for all split flys.

Allowing this at a per-frame level (and not only per-doc level) is
suggested in <https://issues.oasis-open.org/browse/OFFICE-4150>. Given
that this floating table proposal is coming from us, it makes sense to
also support this additional attribute from the proposal, even if it's
not supported by Word.

Fix this by adding a new SwFormatWrapTextAtFlyStart property on fly
frames: this is meant to be enabled when wrap-on-all-pages is wanted for
this fly, but it's disabled at a per-doc level.

Unlike SwFormatFlySplit, this is meant to be easy at a layout level,
since SwFlyAtContentFrame::IsWrapOnAllPages() is already a per-frame
function, it is just backed with a per-doc setting.

Change-Id: I8f562102c2bc366a36c08895b681c4a2ab256bd9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159847
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/inc/hintids.hxx
@@ -122,6 +122,7 @@ class SwFormatNoBalancedColumns;
 class SwFormatPageDesc;
 class SwFormatRowSplit;
 class SwFormatFlySplit;
+class SwFormatWrapTextAtFlyStart;
 class SwFormatSurround;
 class SwFormatURL;
 class SwFormatVertOrient;
@@ -379,24 +380,24 @@ constexpr TypedWhichId<SdrTextVertAdjustItem> RES_TEXT_VERT_ADJUST(137);
 constexpr TypedWhichId<SfxBoolItem> RES_BACKGROUND_FULL_SIZE(138);
 constexpr TypedWhichId<SfxBoolItem> RES_RTL_GUTTER(139);
 constexpr TypedWhichId<SfxBoolItem> RES_DECORATIVE(140);
-constexpr sal_uInt16 RES_FRMATR_END(141);
+constexpr TypedWhichId<SwFormatWrapTextAtFlyStart> RES_WRAP_TEXT_AT_FLY_START(141);
+constexpr sal_uInt16 RES_FRMATR_END(142);
 
 constexpr sal_uInt16 RES_GRFATR_BEGIN(RES_FRMATR_END);
 constexpr TypedWhichId<SwMirrorGrf> RES_GRFATR_MIRRORGRF(RES_GRFATR_BEGIN);
-constexpr TypedWhichId<SwCropGrf> RES_GRFATR_CROPGRF(142);
+constexpr TypedWhichId<SwCropGrf> RES_GRFATR_CROPGRF(143);
 
-constexpr TypedWhichId<SwRotationGrf> RES_GRFATR_ROTATION(143);
-constexpr TypedWhichId<SwLuminanceGrf> RES_GRFATR_LUMINANCE(144);
-constexpr TypedWhichId<SwContrastGrf> RES_GRFATR_CONTRAST(145);
-constexpr TypedWhichId<SwChannelRGrf> RES_GRFATR_CHANNELR(146);
-constexpr TypedWhichId<SwChannelGGrf> RES_GRFATR_CHANNELG(147);
-constexpr TypedWhichId<SwChannelBGrf> RES_GRFATR_CHANNELB(148);
-constexpr TypedWhichId<SwGammaGrf> RES_GRFATR_GAMMA(149);
-constexpr TypedWhichId<SwInvertGrf> RES_GRFATR_INVERT(150);
-constexpr TypedWhichId<SwTransparencyGrf> RES_GRFATR_TRANSPARENCY(151);
-constexpr TypedWhichId<SwDrawModeGrf> RES_GRFATR_DRAWMODE(152);
+constexpr TypedWhichId<SwRotationGrf> RES_GRFATR_ROTATION(144);
+constexpr TypedWhichId<SwLuminanceGrf> RES_GRFATR_LUMINANCE(145);
+constexpr TypedWhichId<SwContrastGrf> RES_GRFATR_CONTRAST(146);
+constexpr TypedWhichId<SwChannelRGrf> RES_GRFATR_CHANNELR(147);
+constexpr TypedWhichId<SwChannelGGrf> RES_GRFATR_CHANNELG(148);
+constexpr TypedWhichId<SwChannelBGrf> RES_GRFATR_CHANNELB(149);
+constexpr TypedWhichId<SwGammaGrf> RES_GRFATR_GAMMA(150);
+constexpr TypedWhichId<SwInvertGrf> RES_GRFATR_INVERT(151);
+constexpr TypedWhichId<SwTransparencyGrf> RES_GRFATR_TRANSPARENCY(152);
+constexpr TypedWhichId<SwDrawModeGrf> RES_GRFATR_DRAWMODE(153);
 
-constexpr TypedWhichId<SfxBoolItem> RES_GRFATR_DUMMY3(153);
 constexpr TypedWhichId<SfxBoolItem> RES_GRFATR_DUMMY4(154);
 constexpr TypedWhichId<SfxBoolItem> RES_GRFATR_DUMMY5(155);
 constexpr sal_uInt16 RES_GRFATR_END(156);

MODAODF/MODA_Office

(cherry picked from commit b1b0cc1b0bb473155b5b089199ca99bb1dc40e42)
Conflicts:
	sw/inc/hintids.hxx
	sw/source/core/bastyp/init.cxx
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159917
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

sw/inc/hintids.hxx
@@ -117,6 +117,7 @@ class SwFormatNoBalancedColumns;
 class SwFormatPageDesc;
 class SwFormatRowSplit;
 class SwFormatFlySplit;
+class SwFormatWrapTextAtFlyStart;
 class SwFormatSurround;
 class SwFormatURL;
 class SwFormatVertOrient;
@@ -371,24 +372,24 @@ constexpr TypedWhichId<SdrTextVertAdjustItem> RES_TEXT_VERT_ADJUST(131);
 constexpr TypedWhichId<SfxBoolItem> RES_BACKGROUND_FULL_SIZE(132);
 constexpr TypedWhichId<SfxBoolItem> RES_RTL_GUTTER(133);
 constexpr TypedWhichId<SfxBoolItem> RES_DECORATIVE(134);
-constexpr sal_uInt16 RES_FRMATR_END(135);
+constexpr TypedWhichId<SwFormatWrapTextAtFlyStart> RES_WRAP_TEXT_AT_FLY_START(135);
+constexpr sal_uInt16 RES_FRMATR_END(136);
 
 constexpr sal_uInt16 RES_GRFATR_BEGIN(RES_FRMATR_END);
 constexpr TypedWhichId<SwMirrorGrf> RES_GRFATR_MIRRORGRF(RES_GRFATR_BEGIN);
-constexpr TypedWhichId<SwCropGrf> RES_GRFATR_CROPGRF(136);
+constexpr TypedWhichId<SwCropGrf> RES_GRFATR_CROPGRF(137);
 
-constexpr TypedWhichId<SwRotationGrf> RES_GRFATR_ROTATION(137);
-constexpr TypedWhichId<SwLuminanceGrf> RES_GRFATR_LUMINANCE(138);
-constexpr TypedWhichId<SwContrastGrf> RES_GRFATR_CONTRAST(139);
-constexpr TypedWhichId<SwChannelRGrf> RES_GRFATR_CHANNELR(140);
-constexpr TypedWhichId<SwChannelGGrf> RES_GRFATR_CHANNELG(141);
-constexpr TypedWhichId<SwChannelBGrf> RES_GRFATR_CHANNELB(142);
-constexpr TypedWhichId<SwGammaGrf> RES_GRFATR_GAMMA(143);
-constexpr TypedWhichId<SwInvertGrf> RES_GRFATR_INVERT(144);
-constexpr TypedWhichId<SwTransparencyGrf> RES_GRFATR_TRANSPARENCY(145);
-constexpr TypedWhichId<SwDrawModeGrf> RES_GRFATR_DRAWMODE(146);
+constexpr TypedWhichId<SwRotationGrf> RES_GRFATR_ROTATION(138);
+constexpr TypedWhichId<SwLuminanceGrf> RES_GRFATR_LUMINANCE(139);
+constexpr TypedWhichId<SwContrastGrf> RES_GRFATR_CONTRAST(140);
+constexpr TypedWhichId<SwChannelRGrf> RES_GRFATR_CHANNELR(141);
+constexpr TypedWhichId<SwChannelGGrf> RES_GRFATR_CHANNELG(142);
+constexpr TypedWhichId<SwChannelBGrf> RES_GRFATR_CHANNELB(143);
+constexpr TypedWhichId<SwGammaGrf> RES_GRFATR_GAMMA(144);
+constexpr TypedWhichId<SwInvertGrf> RES_GRFATR_INVERT(145);
+constexpr TypedWhichId<SwTransparencyGrf> RES_GRFATR_TRANSPARENCY(146);
+constexpr TypedWhichId<SwDrawModeGrf> RES_GRFATR_DRAWMODE(147);
 
-constexpr TypedWhichId<SfxBoolItem> RES_GRFATR_DUMMY3(147);
 constexpr TypedWhichId<SfxBoolItem> RES_GRFATR_DUMMY4(148);
 constexpr TypedWhichId<SfxBoolItem> RES_GRFATR_DUMMY5(149);
 constexpr sal_uInt16 RES_GRFATR_END(150);