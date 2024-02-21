LibreOffice/core

tdf#156829 PPTX import: fix connector regression at missing glue points
Glue points weren't imported if the custom shape type is
"non-primitive", resulting broken line connectors, moreover
broken graphic design, as attached to the original bug report.

~Regression from commit cbf66ec3e60d07efb7c3cceed9b4f0fb4f0510c8
"tdf#89449 PPTX import: fix line connectors".

Change-Id: I9908a23f8e6997bc1384aaeb3e8ba43c08d20d42
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156171
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

svx/source/customshapes/EnhancedCustomShape2d.cxx
@@ -3034,7 +3034,7 @@ void EnhancedCustomShape2d::ApplyGluePoints( SdrObject* pObj )
     {
         SdrGluePoint aGluePoint;
 
-        aGluePoint.SetPos( GetPoint( rGluePoint, true, true ) );
+        aGluePoint.SetPos( GetPoint( rGluePoint, !m_bOOXMLShape, true ) );
         aGluePoint.SetPercent( false );
         aGluePoint.SetAlign( SdrAlign::VERT_TOP | SdrAlign::HORZ_LEFT );
         aGluePoint.SetEscDir( SdrEscapeDirection::SMART );

MODAODF/MODA_Office

(cherry picked from commit a126f499bd4bef203f26b251d85ee30ce5dc4f91)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/156222
Tested-by: Jenkins
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
(cherry picked from commit d13505b8ef7459d97e9f90ee099ca54354d99922)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163609
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Aron Budea <aron.budea@collabora.com>

svx/source/customshapes/EnhancedCustomShape2d.cxx
@@ -3034,7 +3034,7 @@ void EnhancedCustomShape2d::ApplyGluePoints( SdrObject* pObj )
     {
         SdrGluePoint aGluePoint;
 
-        aGluePoint.SetPos( GetPoint( rGluePoint, true, true ) );
+        aGluePoint.SetPos( GetPoint( rGluePoint, !bOOXMLShape, true ) );
         aGluePoint.SetPercent( false );
         aGluePoint.SetAlign( SdrAlign::VERT_TOP | SdrAlign::HORZ_LEFT );
         aGluePoint.SetEscDir( SdrEscapeDirection::SMART );