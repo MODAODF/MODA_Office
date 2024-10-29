LibreOffice/core

Avoid clipboard messages after Scheduler::ImplDeInitScheduler
When running UITests on Windows with parallelism, often there are
clipboard "content changed" messages that fail the assertion in
Scheduler::ImplDeInitScheduler. This is because the clipboard is
only uninitialized after the scheduler de-init; and even then,
the respective threads are not stopped yet.

Make sure that clipboard listeners are stopped before calling the
Scheduler::ImplDeInitScheduler in DeInitVCL.

Change-Id: I37918cdc565c7f1b3bd4f46e71c55bb5a807db6b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/160308
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

vcl/source/app/svmain.cxx
@@ -507,15 +507,6 @@ void DeInitVCL()
     // as this processes all pending events in debug builds.
     ImplGetSystemDependentDataManager().flushAll();
 
-    Scheduler::ImplDeInitScheduler();
-
-    pSVData->mpWinData->maMsgBoxImgList.clear();
-    pSVData->maCtrlData.maCheckImgList.clear();
-    pSVData->maCtrlData.maRadioImgList.clear();
-    pSVData->maCtrlData.moDisclosurePlus.reset();
-    pSVData->maCtrlData.moDisclosureMinus.reset();
-    pSVData->mpDefaultWin.disposeAndClear();
-
 #if defined _WIN32
     // See GetSystemClipboard (vcl/source/treelist/transfer2.cxx):
     if (auto const comp = css::uno::Reference<css::lang::XComponent>(
@@ -526,6 +517,15 @@ void DeInitVCL()
     }
     pSVData->m_xSystemClipboard.clear();
 #endif
+
+    Scheduler::ImplDeInitScheduler();
+
+    pSVData->mpWinData->maMsgBoxImgList.clear();
+    pSVData->maCtrlData.maCheckImgList.clear();
+    pSVData->maCtrlData.maRadioImgList.clear();
+    pSVData->maCtrlData.moDisclosurePlus.reset();
+    pSVData->maCtrlData.moDisclosureMinus.reset();
+    pSVData->mpDefaultWin.disposeAndClear();
 
 #ifndef NDEBUG
     DbgGUIDeInitSolarMutexCheck();

MODAODF/MODA_Office

vcl/source/app/svmain.cxx
@@ -506,15 +506,6 @@ void DeInitVCL()
     // as this processes all pending events in debug builds.
     ImplGetSystemDependentDataManager().flushAll();
 
-    Scheduler::ImplDeInitScheduler();
-
-    pSVData->mpWinData->maMsgBoxImgList.clear();
-    pSVData->maCtrlData.maCheckImgList.clear();
-    pSVData->maCtrlData.maRadioImgList.clear();
-    pSVData->maCtrlData.mpDisclosurePlus.reset();
-    pSVData->maCtrlData.mpDisclosureMinus.reset();
-    pSVData->mpDefaultWin.disposeAndClear();
-
 #if defined _WIN32
     // See GetSystemClipboard (vcl/source/treelist/transfer2.cxx):
     if (auto const comp = css::uno::Reference<css::lang::XComponent>(
@@ -525,6 +516,15 @@ void DeInitVCL()
     }
     pSVData->m_xSystemClipboard.clear();
 #endif
+
+    Scheduler::ImplDeInitScheduler();
+
+    pSVData->mpWinData->maMsgBoxImgList.clear();
+    pSVData->maCtrlData.maCheckImgList.clear();
+    pSVData->maCtrlData.maRadioImgList.clear();
+    pSVData->maCtrlData.mpDisclosurePlus.reset();
+    pSVData->maCtrlData.mpDisclosureMinus.reset();
+    pSVData->mpDefaultWin.disposeAndClear();
 
 #ifndef NDEBUG
     DbgGUIDeInitSolarMutexCheck();