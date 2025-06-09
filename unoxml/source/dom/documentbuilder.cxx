c283462d68042ef442140b2a188a56da4fc7c01b
Xisco Fauli <xiscofauli@libreoffice.org>
2025-06-09 10:12:15 +0200

    libxml2: replace deprecated lastError with xmlCtxtGetLastError
    Follow-up to
    commit e3b5e3aebcf178ccfbba3f7510062d5f2c5abb4a
    Author: Xisco Fauli <xiscofauli@libreoffice.org>
    Date:   Wed Apr 23 13:38:50 2025 +0200
    
        libxml2: upgrade to 2.14.3
    
    Change-Id: I10acc368d9a0dd6db2f6569b2f174583d91dcf35
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/186285
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins
    (cherry picked from commit feb5d010a69c8922574cb8b673f4c147a9bc0bd0)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192524
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    (cherry picked from commit 2e9266452625825e4cc2f7ffc167b5c141cbe626)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/7465
    Tested-by: Michael Stahl <michael.stahl@collabora.com>

--- unoxml/source/dom/documentbuilder.cxx
+++ unoxml/source/dom/documentbuilder.cxx
@@ -137,11 +137,12 @@ namespace DOM
 
     static OUString make_error_message(xmlParserCtxtPtr ctxt)
     {
-        return OUString(ctxt->lastError.message, strlen(ctxt->lastError.message), RTL_TEXTENCODING_ASCII_US) +
+        const xmlError* lastError = xmlCtxtGetLastError(ctxt);
+        return OUString(lastError->message, strlen(lastError->message), RTL_TEXTENCODING_ASCII_US) +
                "Line: " +
-               OUString::number(static_cast<sal_Int32>(ctxt->lastError.line)) +
+               OUString::number(static_cast<sal_Int32>(lastError->line)) +
                "\nColumn: " +
-               OUString::number(static_cast<sal_Int32>(ctxt->lastError.int2));
+               OUString::number(static_cast<sal_Int32>(lastError->int2));
     }
 
     // -- callbacks and context struct for parsing from stream
@@ -257,8 +258,9 @@ namespace DOM
             if (pDocBuilder->getErrorHandler().is())   // if custom error handler is set (using setErrorHandler ())
             {
                 // Prepare SAXParseException to be passed to custom XErrorHandler::warning function
+                const xmlError* lastError = xmlCtxtGetLastError(pctx);
                 css::xml::sax::SAXParseException saxex(make_error_message(pctx), {}, {}, {}, {},
-                                                       pctx->lastError.line, pctx->lastError.int2);
+                                                       lastError->line, lastError->int2);
 
                 // Call custom warning function
                 pDocBuilder->getErrorHandler()->warning(::css::uno::Any(saxex));
@@ -287,8 +289,9 @@ namespace DOM
             if (pDocBuilder->getErrorHandler().is())   // if custom error handler is set (using setErrorHandler ())
             {
                 // Prepare SAXParseException to be passed to custom XErrorHandler::error function
+                const xmlError* lastError = xmlCtxtGetLastError(pctx);
                 css::xml::sax::SAXParseException saxex(make_error_message(pctx), {}, {}, {}, {},
-                                                       pctx->lastError.line, pctx->lastError.int2);
+                                                       lastError->line, lastError->int2);
 
                 // Call custom warning function
                 pDocBuilder->getErrorHandler()->error(::css::uno::Any(saxex));
@@ -304,8 +307,9 @@ namespace DOM
 
     static void throwEx(xmlParserCtxtPtr ctxt)
     {
+        const xmlError* lastError = xmlCtxtGetLastError(ctxt);
         css::xml::sax::SAXParseException saxex(make_error_message(ctxt), {}, {}, {}, {},
-                                               ctxt->lastError.line, ctxt->lastError.int2);
+                                               lastError->line, lastError->int2);
         throw saxex;
     }
 
