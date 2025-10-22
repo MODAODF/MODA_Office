8ac59c1b7cc1587a660bef0d19482b83d5232355
Xisco Fauli <xiscofauli@libreoffice.org>
2025-10-22 10:31:23 +0200

    libxml2: migrate to CMake on Windows
    In preparation for the upgrade to 2.15
    
    Change-Id: I439cf6d1d113d97b5f23421da018736305eb683e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192827
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins
    (cherry picked from commit 3bb1efbee44b30cd4b3869df99def745aef39ae4)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/7188
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    Tested-by: Michael Stahl <michael.stahl@collabora.com>
    (cherry picked from commit bc81752fc8e38ff98da4e100c7e120c768b04620)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/7318
    (cherry picked from commit c6d8cb0c05affd9ad5995ab13fffbb113514a3bf)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/7472

--- external/libxslt/ExternalProject_libxslt.mk
+++ external/libxslt/ExternalProject_libxslt.mk
@@ -24,7 +24,7 @@ $(call gb_ExternalProject_get_state_target,libxslt,build):
 			$(if $(MSVC_USE_DEBUG_RUNTIME),cruntime=/MDd) \
 			$(if $(filter TRUE,$(ENABLE_DBGUTIL)),debug=yes) \
 			vcmanifest=yes \
-			lib=$(call gb_UnpackedTarball_get_dir,libxml2)/win32/bin.msvc \
+			lib=$(call gb_UnpackedTarball_get_dir,libxml2)/$(if $(MSVC_USE_DEBUG_RUNTIME),Debug,Release) \
 		&& nmake \
 	,win32)
 	$(call gb_Trace_EndRange,libxslt,EXTERNAL)
--- external/xmlsec/ExternalProject_xmlsec.mk
+++ external/xmlsec/ExternalProject_xmlsec.mk
@@ -33,9 +33,9 @@ $(call gb_ExternalProject_get_state_target,xmlsec,build) :
 	$(call gb_Trace_StartRange,xmlsec,EXTERNAL)
 	$(call gb_ExternalProject_run,build,\
 		cscript /e:javascript configure.js crypto=mscng xslt=no iconv=no static=no \
-			lib=$(call gb_UnpackedTarball_get_dir,libxml2)/win32/bin.msvc \
+			lib=$(call gb_UnpackedTarball_get_dir,libxml2)/$(if $(MSVC_USE_DEBUG_RUNTIME),Debug,Release) \
 			$(if $(filter TRUE,$(ENABLE_DBGUTIL)),debug=yes cruntime=/MDd) \
-			cflags="$(SOLARINC) -I$(WORKDIR)/UnpackedTarball/libxml2/include -I$(WORKDIR)/UnpackedTarball/icu/source/i18n -I$(WORKDIR)/UnpackedTarball/icu/source/common" \
+			cflags="$(SOLARINC) $(LIBXML_CFLAGS)" \
 		&& nmake \
 	,win32)
 	$(call gb_Trace_EndRange,xmlsec,EXTERNAL)
