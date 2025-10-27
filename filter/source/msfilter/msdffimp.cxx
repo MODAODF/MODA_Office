4dde34e4f7ec0af1ae6beb6cabb2aa7e4d9e751a
Stephan Bergmann <stephan.bergmann@collabora.com>
2025-10-27 22:23:44 +0100

    Silence -Werror=maybe-uninitialized
    ...as seen at least with --disable-optimized against recent GCC 16 trunk,
    
    > filter/source/msfilter/msdffimp.cxx:5810:24: error: ‘<unknown>’ may be used uninitialized [-Werror=maybe-uninitialized]
    >  5810 |      mbSkipImages(false)
    >       |                        ^
    > filter/source/msfilter/msdffimp.cxx:239:1: note: by argument 2 of type ‘const SvxMSDffManager&’ to ‘DffPropertyReader::DffPropertyReader(const SvxMSDffManager&)’ declared here
    >   239 | DffPropertyReader::DffPropertyReader( const SvxMSDffManager& rMan )
    >       | ^~~~~~~~~~~~~~~~~
    > filter/source/msfilter/msdffimp.cxx: In constructor ‘SvxMSDffManager::SvxMSDffManager(SvStream&, rtl::OUString, sal_uInt32, SvStream*, SdrModel*, tools::Long, Color, SvStream*, bool)’:
    > filter/source/msfilter/msdffimp.cxx:5769:31: error: ‘<unknown>’ may be used uninitialized [-Werror=maybe-uninitialized]
    >  5769 |      mbSkipImages (bSkipImages)
    >       |                               ^
    > filter/source/msfilter/msdffimp.cxx:239:1: note: by argument 2 of type ‘const SvxMSDffManager&’ to ‘DffPropertyReader::DffPropertyReader(const SvxMSDffManager&)’ declared here
    >   239 | DffPropertyReader::DffPropertyReader( const SvxMSDffManager& rMan )
    >       | ^~~~~~~~~~~~~~~~~
    
    Change-Id: Ib1cb233e0208c8316db399c369a4879bca5b31eb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193058
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    (cherry picked from commit b792c423259c4bc475ed075d0e1abc8942e8eb0d)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/5961
    Tested-by: Michael Stahl <michael.stahl@collabora.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    (cherry picked from commit cef00fa30214b5a4e161e1b540a1cd8c507aa85f)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/7452

--- filter/source/msfilter/msdffimp.cxx
+++ filter/source/msfilter/msdffimp.cxx
@@ -5766,6 +5766,10 @@ void SvxMSDffManager::RemoveFromShapeOrder( SdrObject const * pObject ) const
 
 //  exported class: Public Methods
 
+#if defined __GNUC__ && !defined __clang__ && __GNUC__ == 16
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
 SvxMSDffManager::SvxMSDffManager(SvStream& rStCtrl_,
                                  OUString aBaseURL,
                                  sal_uInt32 nOffsDgg_,
@@ -5816,7 +5820,14 @@ SvxMSDffManager::SvxMSDffManager(SvStream& rStCtrl_,
     if( &rStCtrl != pStData )
         pStData->Seek( nOldPosData );
 }
+#if defined __GNUC__ && !defined __clang__ && __GNUC__ == 16
+#pragma GCC diagnostic pop
+#endif
 
+#if defined __GNUC__ && !defined __clang__ && __GNUC__ == 16
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
 SvxMSDffManager::SvxMSDffManager( SvStream& rStCtrl_, OUString aBaseURL )
     :DffPropertyReader( *this ),
      m_pBLIPInfos( new SvxMSDffBLIPInfos ),
@@ -5836,6 +5847,9 @@ SvxMSDffManager::SvxMSDffManager( SvStream& rStCtrl_, OUString aBaseURL )
 {
     SetModel( nullptr, 0 );
 }
+#if defined __GNUC__ && !defined __clang__ && __GNUC__ == 16
+#pragma GCC diagnostic pop
+#endif
 
 SvxMSDffManager::~SvxMSDffManager()
 {
