13a21808c209fea8da1987ed6c5fcd90e06977dc
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-07-19 10:55:32 +0000

    ofz#535775262 Out-of-memory
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I4c7b8208df6ef7da663ad68aac5cdf119c96a3c4
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/7262
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/7272
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit 7bbe72cd3bdced0c8617fc0246bea9ed5a09332f)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/7633
    (cherry picked from commit 771e094dd840e33955dc8263225a2ed12242547f)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/9685

--- vcl/source/fontsubset/cff.cxx
+++ vcl/source/fontsubset/cff.cxx
@@ -17,6 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <algorithm>
 #include <cstdio>
 #include <cstring>
 #include <vector>
@@ -2483,7 +2484,7 @@ bool CffSubsetterContext::convertCharStrings(const sal_GlyphId* pGlyphIds, int n
     // If we are doing extra glyphs used for seac operator, check for already
     // converted glyphs.
     bool bCheckDuplicates = !rCharStrings.empty();
-    rCharStrings.reserve(rCharStrings.size() + nGlyphCount);
+    rCharStrings.reserve(rCharStrings.size() + std::min<size_t>(nGlyphCount, mpBaseEnd - mpBasePtr));
     for (int i = 0; i < nGlyphCount; ++i)
     {
         const int nCffGlyphId = pGlyphIds[i];
