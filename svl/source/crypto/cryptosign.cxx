0b422431fb8a6658e24b0979d13f3920312a9068
Michael Stahl <michael.stahl@collabora.com>
2026-07-30 13:49:58 +0200

    oox,svl,vcl: use more appropriate random numbers
    Signed-off-by: Michael Stahl <michael.stahl@collabora.com>
    Change-Id: I9d6f200cc5a146f46dca64af270b77d0ce6abf6b
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/8174
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    (cherry picked from commit 52ba487c3d84a89a027c3130bc86afdcfe57a860)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/8261
    Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    (cherry picked from commit 63ab1f905bddbac4871a246d61aa59d09f122e5c)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/8277

--- svl/source/crypto/cryptosign.cxx
+++ svl/source/crypto/cryptosign.cxx
@@ -20,6 +20,7 @@
 #endif
 
 #include <rtl/character.hxx>
+#include <rtl/random.h>
 #include <rtl/strbuf.hxx>
 #include <rtl/string.hxx>
 #include <sal/log.hxx>
@@ -28,7 +29,6 @@
 #include <comphelper/base64.hxx>
 #include <comphelper/hash.hxx>
 #include <comphelper/processfactory.hxx>
-#include <comphelper/random.hxx>
 #include <comphelper/scopeguard.hxx>
 #include <comphelper/lok.hxx>
 #include <com/sun/star/security/XCertificate.hpp>
@@ -1063,7 +1063,8 @@ bool Signing::Sign(OStringBuffer& rCMSHexBuffer)
         src.reqPolicy.data = nullptr;
         src.reqPolicy.len = 0;
 
-        unsigned int nNonce = comphelper::rng::uniform_uint_distribution(0, SAL_MAX_UINT32);
+        unsigned int nNonce;
+        rtl_random_getBytes(nullptr, &nNonce, sizeof(nNonce));
         src.nonce.type = siUnsignedInteger;
         src.nonce.data = reinterpret_cast<unsigned char*>(&nNonce);
         src.nonce.len = sizeof(nNonce);
@@ -1543,7 +1544,8 @@ bool Signing::Sign(OStringBuffer& rCMSHexBuffer)
         CMSG_SIGNER_INFO *pDecodedSignerInfo = reinterpret_cast<CMSG_SIGNER_INFO *>(pDecodedSignerInfoBuf.get());
 
         CRYPT_TIMESTAMP_PARA aTsPara;
-        unsigned int nNonce = comphelper::rng::uniform_uint_distribution(0, SAL_MAX_UINT32);
+        unsigned int nNonce;
+        rtl_random_getBytes(nullptr, &nNonce, sizeof(nNonce));
 
         aTsPara.pszTSAPolicyId = nullptr;
         aTsPara.fRequestCerts = TRUE;
